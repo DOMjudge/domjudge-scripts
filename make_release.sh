53a1bb473cfad00fc82d99655ac9dee9a3ebe5b1
Jaap Eldering <eldering@a-eskwadraat.nl>
2010-03-14 16:39:58 +0000

    Add release and snapshot scripts.
    git-svn-id: https://secure.a-eskwadraat.nl/svn/domjudge/scripts@3161 caf94f87-7bd6-0310-88dc-dee8202e2269

---
+++ make_release.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# $Id$
+#
+# Script to create a DOMjudge release package. Release file is
+# generated in the current directory.
+
+TEMPDIR=`mktemp -d /tmp/domjudge.XXXXXX`
+
+svn -q export https://secure.a-eskwadraat.nl/svn/domjudge/trunk $TEMPDIR/domjudge
+
+OWD="$PWD"
+
+cd $TEMPDIR/domjudge
+
+VERSION="`cat README | head -n 1 | sed 's/.*version //'`"
+CHLOG="`grep ^Version ChangeLog | head -n 1`"
+
+# Ignore libmcrypt warnings when running autoconf.
+make QUIET=1 dist 2>&1 | grep -v libmcrypt.m4
+
+cd ..
+
+mv domjudge domjudge-$VERSION
+
+tar -cf "domjudge-$VERSION.tar" "domjudge-$VERSION"
+gzip -9 "domjudge-$VERSION.tar"
+
+cd "$OWD"
+
+mv $TEMPDIR/domjudge-$VERSION.tar.gz .
+rm -rf "$TEMPDIR"
+
+echo "Release file: 'domjudge-$VERSION.tar.gz'"
+echo "ChangeLog version: '$CHLOG'"
+
+exit 0
---
+++ make_snapshot.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# $Id$
+#
+# Script public a snapshot package, ChangeLog en admin-manual on the
+# public page at http://domjudge.sourceforge.net/snapshot/.
+
+#DEBUG=1
+
+PUBDIR=~/public_html/snapshot
+
+[ "$DEBUG" ] && set -x
+quiet()
+{
+	if [ "$DEBUG" ]; then
+		$@
+	else
+		$@ &> /dev/null
+	fi
+}
+
+TEMPDIR=`mktemp -d /tmp/domjudge.XXXXXX`
+cd $TEMPDIR
+
+DATE=`date +%Y%m%d`
+
+rm -rf $PUBDIR/*
+
+svn export -q https://secure.a-eskwadraat.nl/svn/domjudge/trunk domjudge-snapshot-$DATE
+
+quiet make -C domjudge-snapshot-$DATE dist
+tar -cf domjudge-snapshot-$DATE.tar domjudge-snapshot-$DATE
+gzip -9 domjudge-snapshot-$DATE.tar
+cp domjudge-snapshot-$DATE.tar.gz    $PUBDIR
+cp domjudge-snapshot-$DATE/ChangeLog $PUBDIR
+
+quiet make -C domjudge-snapshot-$DATE docs
+mkdir -p $PUBDIR/admin-manual $PUBDIR/judge-manual
+cp domjudge-snapshot-$DATE/doc/admin/{admin-manual*.{html,pdf},*.png} $PUBDIR/admin-manual/
+cp domjudge-snapshot-$DATE/doc/judge/judge-manual*.{html,pdf}         $PUBDIR/judge-manual/
+cp domjudge-snapshot-$DATE/doc/team/team-manual.pdf                   $PUBDIR/
+cd /
+
+[ "$DEBUG" ] || rm -rf $TEMPDIR
+
+exit 0
