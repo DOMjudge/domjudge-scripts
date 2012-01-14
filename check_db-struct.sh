94760c8c7d5093483b72f1040e4d2f56769b4a4d
Jaap Eldering <eldering@a-eskwadraat.nl>
2012-01-14 12:35:02 +0100

    Switch from SVN to git repository to obtain MySQL structure file.

--- check_db-struct.sh
+++ check_db-struct.sh
@@ -4,17 +4,18 @@
 # Usage: $0 [<local-sql-file> [<databasename>]]
 #
 # Check whether DOMjudge mysql database structure is consistent with
-# svn repository: outputs diff.
+# git repository: outputs diff.
 
 set -e
-
-SVNURL='https://secure.a-eskwadraat.nl/svn/domjudge/trunk/sql/mysql_db_structure.sql'
 
 MYSQLOPTS='-u domjudge_jury -p'
 DBNAME='domjudge'
 
+# For some reason read-only access via git:// doesn't work
+GITURL='ssh://domjudge@a-eskwadraat.nl/home/extern/domjudge/git/domjudge.git'
+
 DBSTRUCT=` mktemp /tmp/domjudge-db__sql.XXXXXX`
-SVNSTRUCT=`mktemp /tmp/domjudge-svn_sql.XXXXXX`
+GITSTRUCT=`mktemp /tmp/domjudge-git_sql.XXXXXX`
 SQLTMP=`   mktemp /tmp/domjudge-tmp_sql.XXXXXX`
 
 # Filter SQL expression for comments and unimportant changes
@@ -39,10 +40,11 @@ mysqldump $MYSQLOPTS -n -d -Q --skip-add-drop-table "$DBNAME" | \
 if [ -n "$LOCALSQL" ]; then
 	cp "$LOCALSQL" $SQLTMP
 else
-	svn export -q --non-interactive "$SVNURL" $SQLTMP
+	git archive --format=tar --remote=$GITURL master \
+		sql/mysql_db_structure.sql 2>/dev/null | tar x -O > $SQLTMP
 fi
-cat $SQLTMP | sqlfilter > $SVNSTRUCT
+cat $SQLTMP | sqlfilter > $GITSTRUCT
 
-diff -ibw -u $DBSTRUCT $SVNSTRUCT || true
+diff -ibw -u $DBSTRUCT $GITSTRUCT || true
 
-rm -f $SQLTMP $DBSTRUCT $SVNSTRUCT
+rm -f $SQLTMP $DBSTRUCT $GITSTRUCT
