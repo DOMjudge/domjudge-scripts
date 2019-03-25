e83f348b4ad65efab3f8180a5026a25be1cd91f8
DOMjudge <domjudge@domjudge.org>
2019-03-25 22:53:48 +0100

    Add demoweb dump script and readme how to update demoweb DB.

---
+++ dump_demoweb_db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Script to dump the DOMjudge demo web DB to ~/demoweb/domjudge_
+# at:    https://www.domjudge.org/demoweb/
+
+set -e
+
+DBNAME='domjudge_demo'
+DUMPFILE=~/demoweb/${DBNAME}-$(date +%F).sql
+
+mysqldump $DBNAME | process-mysqldump > $DUMPFILE
+
+cat <<EOF
+
+Dumped demoweb DB in '$DUMPFILE'.
+
+Don't forget to diff the contents and update
+the ~/demoweb/domjudge_demo.sql symlink.
+EOF
