0fed1c861da25cd7f7b7c2c8b802d94eea6370d2
DOMjudge team <team@domjudge.org>
2019-03-31 16:12:49 +0100

    Add dump-api script.

---
+++ icpc-wf/dump-api.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+contest=$1
+where=$2
+baseurl="https://domjudge/api/contests"
+
+mkdir -p "$where"
+
+for endpoint in judgement-types languages problems groups organizations teams state submissions judgements runs clarifications scoreboard; do
+	http GET "$baseurl/$contest/$endpoint" | python -mjson.tool > "$where/${endpoint}.json"
+done
+endpoint=contest
+http GET "$baseurl/$contest" | python -mjson.tool > "$where/${endpoint}.json"
+endpoint=event-feed
+http GET "$baseurl/$contest/${endpoint}?stream=0" > "$where/${endpoint}.json"
