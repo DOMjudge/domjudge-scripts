d979202329bc8d28dece5745ede522dbb63ffb48
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-02-27 23:01:50 +0100

    Cronjob to release on new tags
    The last addition to the make_release is not needed anymore as
    deployment is now done via the server.
    
    - Setup extra environment variables
    - Trigger tests on gitlab
    - GPG tag is checked against local keyring on the server
    - Script can be used also in GitLab to do a trial run
    - When there is an unknown signer for a tag (or not signed) SLACK will
    be alerted. This should only run once.
    
    In the future the docs can also be updated.

--- make_release.sh
+++ make_release.sh
@@ -3,12 +3,13 @@
 # Script to create a DOMjudge release package. Release file is
 # generated in the current directory.
 
-if [ -z "${CI+x}" ]; then
+CI_USAGE=${CI+x}
+if [ -z "$CI_USAGE" ]; then
     set -e
 else
     set -eux
     export PS4='(${0}:): - [$?] $ '
-fi
+fi 
 
 GITURL="https://github.com/DOMjudge/domjudge.git"
 
@@ -19,11 +20,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if [ -n "${CI+x}" ]; then
+if [ -n "$CI_USAGE" ]; then
     set +x
 fi
 TAG="$1" ; shift
-if [ -n "${CI+x}" ]; then
+if [ -n "$CI_USAGE" ]; then
     set -x
 fi
 
@@ -53,6 +54,10 @@ CHLOG_VERSION="$(echo "$CHLOG" | sed -r 's/^Version ([0-9\.]+) .*$/\1/')"
 if [ "$VERSION" != "$CHLOG_VERSION" ]; then
     echo "WARNING: version strings in README* and ChangeLog differ:"
     echo "'$VERSION' != '$CHLOG_VERSION'"
+    if [ ! -t 1 && -z ${CI+x} ] ; then
+        # In the cronjob this would be reason to not release
+        exit 1
+    fi
 fi
 
 # Add released tag for revision information:
@@ -64,17 +69,17 @@ cd ..
 
 mv domjudge "domjudge-$VERSION"
 
-tar -cf "domjudge-$VERSION.tar" "domjudge-$VERSION"
-gzip -9 "domjudge-$VERSION.tar"
+tar -cf "domjudge-$TAG.tar" "domjudge-$VERSION"
+gzip -9 "domjudge-$TAG.tar"
 
 cd "$OWD"
 
-mv "$TEMPDIR/domjudge-$VERSION.tar.gz" .
+mv "$TEMPDIR/domjudge-$TAG.tar.gz" .
 rm -rf "$TEMPDIR"
 
-sha256sum "domjudge-$VERSION.tar.gz" > "domjudge-$VERSION.tar.gz.sha256sum"
+sha256sum "domjudge-$TAG.tar.gz" > "domjudge-$TAG.tar.gz.sha256sum"
 
-GPG_ARGS="-a --detach-sign --digest-algo SHA256 domjudge-$VERSION.tar.gz"
+GPG_ARGS="-a --detach-sign --digest-algo SHA256 domjudge-$TAG.tar.gz"
 if [ -t 1 ] ; then
     # Explicit not quoted!
     gpg $GPG_ARGS
