7758d6731a339136da11f925313b413ac3251f21
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-02-27 23:01:50 +0100

    Cronjob to release on new tags
    The last addition to the make_release is not needed anymore as
    deployment is now done via the server.
    
    - Setup extra environment variables
    - Trigger tests on gitlab
    - GPG tag is checked against local keyring on the server
    - Script can be used also in GitLab to do a trial run
    - When there is an unknown signer for a tag (or not signed) SLACK will
    be alerted. This should only run once.
    
    In the future the docs can also be updated.

---
+++ cron_tag_release.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -eu
+
+main=master
+
+RELEASE_DIR="/srv/http/domjudge/releases"
+DOWNLOAD_RELEASE_SCRIPT=""
+
+notify_channel () {
+    # Local debug
+    echo "$1"
+    # When cron is run often one should have time to
+    # fix the issue.
+    if [ ! -f /tmp/$2 ]; then
+        # Write to syslog on server
+        logger "$1"
+        DATA='{"text":"'"$1"'"}'
+        # Notify DOMjudge Slack channel (github-notifications)
+        # SLACK_URL should be exported in the .bashrc (it should be secret)
+        curl -X POST -H 'Content-type: application/json' --data "$DATA" "$SLACK_URL"
+        touch /tmp/$2
+    fi
+}
+
+process_tag () {
+    TAG="$1"
+    NUMB="[0-9]+"
+    DOT="\."
+    RELEASE="$NUMB$DOT$NUMB$DOT$NUMB"
+    OPTRC="((RC|rc)[0-9])?"
+    if [[ $TAG =~ ^$RELEASE$OPTRC$ ]]; then
+       # TODO: check if the file already exists
+       if [ -f "$RELEASE_DIR/domjudge-$TAG.tar.gz" ]; then
+           # Tag is already handled
+           return 0
+       fi
+       # To find the signer key of a earlier tag:
+       # gpg --search 780355B5EA6BFC8235A99C4B56F61A79401DAC04
+       # And if one trusts the internet to be correct
+       # gpg --recv-keys 780355B5EA6BFC8235A99C4B56F61A79401DAC04
+       set +e # Some tags are not signed
+       if git verify-tag $TAG; then
+           set -e
+           # At this point the tarball should already be locally tested
+           ~/domjudge-scripts/make_release.sh "$TAG"
+           mv domjudge-$TAG.* $RELEASE_DIR/
+           notify_channel "Tarball finished ($TAG)." "$TAG"
+       else
+           notify_channel "Untrusted tag ($TAG)" "$TAG"
+       fi
+    fi
+}
+
+# Reset to main branch
+cd ~domjudge/domjudge
+git checkout $main
+
+while read -r tag; do
+    #echo "Handling tag: $tag"
+    process_tag "$tag"
+done <<< "$(git tag)"
+
