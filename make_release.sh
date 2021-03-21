8cbf4f51ef1712eaf443abcb6f36d5d461ba3625
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-03-21 14:14:08 +0100

    Shellcheck and bashate fixes
    Fixed long lines and quoting issues

--- make_release.sh
+++ make_release.sh
@@ -12,25 +12,30 @@ fi
 
 GITURL="https://github.com/DOMjudge/domjudge.git"
 
-TEMPDIR=`mktemp -d /tmp/domjudge-make_release-XXXXXX`
+TEMPDIR=$(mktemp -d /tmp/domjudge-make_release-XXXXXX)
 
 if [ -z "$1" ]; then
-	echo "Error: missing required release tag argument."
-	exit 1
+    echo "Error: missing required release tag argument."
+    exit 1
 fi
 
-if [ ! -z "${CI+x}" ]; then set +x; fi
+if [ -n "${CI+x}" ]; then
+    set +x
+fi
 TAG="$1" ; shift
-if [ ! -z "${CI+x}" ]; then set -x; fi
+if [ -n "${CI+x}" ]; then
+    set -x
+fi
 
 if [ -n "${1+x}" ]; then
     GITURL="$1" ; shift
 fi
 
 OWD="$PWD"
-cd $TEMPDIR
+cd "$TEMPDIR"
 
-git clone -q --no-checkout --depth 1 --single-branch --branch "$TAG" "$GITURL" dj-clone
+git clone -q --no-checkout --depth 1 --single-branch \
+    --branch "$TAG" "$GITURL" dj-clone
 
 ( cd dj-clone && git archive --prefix=domjudge/ --format=tar "$TAG" ) | tar x
 
@@ -44,9 +49,10 @@ if [ "${VERSION%DEV}" != "${VERSION}" ]; then
     echo "WARNING: version string contains 'DEV', should probably be changed!"
 fi
 
-CHLOG_VERSION="$(echo $CHLOG | sed -r 's/^Version ([0-9\.]+) .*$/\1/')"
+CHLOG_VERSION="$(echo "$CHLOG" | sed -r 's/^Version ([0-9\.]+) .*$/\1/')"
 if [ "$VERSION" != "$CHLOG_VERSION" ]; then
-	echo "WARNING: version strings in README* and ChangeLog differ: '$VERSION' != '$CHLOG_VERSION'"
+    echo "WARNING: version strings in README* and ChangeLog differ:"
+    echo "'$VERSION' != '$CHLOG_VERSION'"
 fi
 
 # Add released tag for revision information:
@@ -56,21 +62,24 @@ make QUIET=1 dist
 
 cd ..
 
-mv domjudge domjudge-$VERSION
+mv domjudge "domjudge-$VERSION"
 
 tar -cf "domjudge-$VERSION.tar" "domjudge-$VERSION"
 gzip -9 "domjudge-$VERSION.tar"
 
 cd "$OWD"
 
-mv $TEMPDIR/domjudge-$VERSION.tar.gz .
+mv "$TEMPDIR/domjudge-$VERSION.tar.gz" .
 rm -rf "$TEMPDIR"
 
-sha256sum domjudge-$VERSION.tar.gz > domjudge-$VERSION.tar.gz.sha256sum
-if [ -z "${CI+x}" ]; then
-    gpg -a --detach-sign --digest-algo SHA256 domjudge-$VERSION.tar.gz
+sha256sum "domjudge-$VERSION.tar.gz" > "domjudge-$VERSION.tar.gz.sha256sum"
+
+GPG_ARGS="-a --detach-sign --digest-algo SHA256 domjudge-$VERSION.tar.gz"
+if [ -t 1 ] ; then
+    # Explicit not quoted!
+    gpg $GPG_ARGS
 else
-    gpg --pinentry-mode loopback --batch --passphrase ${PASS} --detach-sign --armor --digest-algo SHA256 domjudge-$VERSION.tar.gz
+    gpg --pinentry-mode=loopback --batch --passphrase-file ~domjudge/sign_key_pass $GPG_ARGS
 fi
 
 echo "Release file: 'domjudge-$VERSION.tar.gz'"
