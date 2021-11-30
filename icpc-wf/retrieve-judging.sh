17408164677e97d51ac013eb0c2bce321ff25b83
Jaap Eldering <jaap@jaapeldering.nl>
2021-11-30 23:03:11 +0100

    More shellcheck ignores and fixes.

--- icpc-wf/retrieve-judging.sh
+++ icpc-wf/retrieve-judging.sh
@@ -2,10 +2,10 @@
 
 jid=$1
 
-judgehost=$(http https://domjudge/api/contests/test1/judgements/${jid} | grep judgehost | cut -d\" -f4 | cut -d- -f1,2)
+judgehost=$(http "https://domjudge/api/contests/test1/judgements/${jid}" | grep judgehost | cut -d\" -f4 | cut -d- -f1,2)
 
 dest="/tmp/j${jid}/"
 mkdir -p "$dest"
-scp -r $judgehost:/home/domjudge/domjudge/output/judgings/${judgehost}-0/endpoint-default/c*j${jid}/* "$dest" &> /dev/null
+scp -r "$judgehost:/home/domjudge/domjudge/output/judgings/${judgehost}-0/endpoint-default/c*j${jid}/*" "$dest" > /dev/null 2>&1
 
 echo "Succesfully copied package to $dest"
