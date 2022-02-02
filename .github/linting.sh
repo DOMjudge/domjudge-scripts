07dd870638ed8f223e69fd29922924933ac75e97
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2022-02-02 21:35:01 +0100

    Test ansible scripts with default linter
    We first ignore all errors and fix those in separate commits.

---
+++ .github/linting.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+set -euxo pipefail
+
+# shellcheck disable=SC2044
+for book in $(find ./ -maxdepth 1 -name "*.yml"); do
+    if [ "$book" != "./handlers.yml" ]; then
+        ansible-lint "$book" -x braces,line-length
+    fi
+done
+# shellcheck disable=SC2044
+for dir in $(find ./roles -maxdepth 1 -type d); do
+    ansible-lint "$dir" -x braces,line-length
+done
