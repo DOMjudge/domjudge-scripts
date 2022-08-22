ea9246e275e55f6dffbc6d685d3e0aaef0fac15b
Nicky Gerritsen <nickygerritsen@me.com>
2022-08-22 09:52:26 +0200

    Fix long line check and add spaces around | for CI happiness.

--- .github/linting.sh
+++ .github/linting.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 # shellcheck disable=SC2044
 for book in $(find ./ -maxdepth 1 -name "*.yml"); do
     if [ "$book" != "./handlers.yml" ]; then
-        ansible-lint "$book" -x braces,line-length
+        ansible-lint "$book" -x 'braces,yaml[line-length]'
     fi
 done
 # shellcheck disable=SC2044
 for dir in $(find ./roles -maxdepth 1 -type d); do
-    ansible-lint "$dir" -x braces,line-length
+    ansible-lint "$dir" -x 'braces,yaml[line-length]'
 done
