56caefdc5835e8ed90f092d13bef54ce92fcc52d
DOMjudge team <team@domjudge.org>
2022-11-06 11:49:38 +0600

    Install local python packags

---
+++ provision-contest/ansible/files/admin-python/site-packages/termcolor/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from termcolor.termcolor import ATTRIBUTES, COLORS, HIGHLIGHTS, RESET, colored, cprint
+
+__all__ = [
+    "ATTRIBUTES",
+    "COLORS",
+    "HIGHLIGHTS",
+    "RESET",
+    "colored",
+    "cprint",
+]
