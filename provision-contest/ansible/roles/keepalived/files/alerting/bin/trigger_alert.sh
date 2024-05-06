f92a0993da9df296b26cd0db7e35d41d840cb592
Tobias Werth <git@never-mind.eu>
2024-05-06 21:59:16 +0200

    Improve keepalived alert script and add some comments (#139)
    At the ICPC WFs in Egypt the correct sound card to use is 0;
    just try to set the volume on both 0 and 1. Also try beep and send
    a desktop notification.
    
    Co-authored-by: DOMjudge team <team@domjudge.org>

--- provision-contest/ansible/roles/keepalived/files/alerting/bin/trigger_alert.sh
+++ provision-contest/ansible/roles/keepalived/files/alerting/bin/trigger_alert.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# This only copies the script below which contains alerting code
+# A desktop autostart application started the alert_listener.sh
+# script, which then detects the copy and executes it.
+
 cp ~/bin/alerting.sh.template ~/alerting.sh
